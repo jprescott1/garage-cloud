3822f2498a395abde50303a211dd362735b5f244
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-05-07 09:56:11 -0500

    setting up modules

---
+++ infra/modules/kubernetes/variables.tf
@@ -0,0 +1,9 @@
+variable "filename" {
+  description = "The name of the file to create"
+  type        = string
+}
+
+variable "content" {
+  description = "The content to write to the file"
+  type        = string
+}
