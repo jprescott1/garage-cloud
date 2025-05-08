ba488d1c14bc75126345b129a43acbeaf234fa99
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-05-08 08:21:25 -0500

    add multi-vm creation

--- infra/modules/lxd-vms/main.tf
+++ infra/modules/lxd-vms/main.tf
@@ -1,14 +1,21 @@
+resource "random_string" "suffix" {
+  count   = var.instance_count
+  length  = 8
+  special = false
+}
+
 resource "lxd_instance" "instance" {
-  name  = var.name
+  count = var.instance_count
+  name  = "${var.name}${count.index + 1}-${random_string.suffix[count.index].result}"
   image = "ubuntu:noble/amd64"
-  type = "virtual-machine"
+  type  = "virtual-machine"
   config = {
     "user.user-data" = file("${path.module}/cloud-init.yaml")
     "boot.autostart" = true
   }
 
   limits = {
-    cpu = var.cpu
+    cpu    = var.cpu
     memory = var.memory
   }
 }
--- infra/modules/lxd-vms/variables.tf
+++ infra/modules/lxd-vms/variables.tf
@@ -3,11 +3,17 @@ variable "name" {
 }
 
 variable "cpu" {
-  type = number
+  type    = number
   default = 2
 }
 
 variable "memory" {
-  type = string
+  type    = string
   default = "4GiB"
+}
+
+variable "instance_count" {
+  description = "Number of instances to create"
+  type        = number
+  default     = 1
 }
