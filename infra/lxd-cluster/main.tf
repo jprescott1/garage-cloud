9ab107074c15661ba11285d2346a811d2fc886ec
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-05-07 19:29:59 -0500

    format

--- infra/lxd-cluster/main.tf
+++ infra/lxd-cluster/main.tf
@@ -6,8 +6,8 @@ terraform {
 }
 
 module "node1" {
-    source = "../modules/lxd-vms"
-    name = "vm-1"
-    cpu = 1
-    memory = "8GiB"
+  source = "../modules/lxd-vms"
+  name   = "vm-1"
+  cpu    = 1
+  memory = "8GiB"
 }
