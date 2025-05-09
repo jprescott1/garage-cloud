d1eff1fe483496f783f3c795e533c499d5f07c82
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-05-09 07:29:11 -0500

    test adding additional vms

--- infra/lxd-cluster/main.tf
+++ infra/lxd-cluster/main.tf
@@ -14,3 +14,12 @@ module "control-plane" {
   memory         = "6GiB"
   volume_size    = "80GiB"
 }
+
+module "worker" {
+  source         = "../modules/lxd-vms"
+  instance_count = 3
+  name_prefix    = "worker"
+  cpu            = 4
+  memory         = "6GiB"
+  volume_size    = "80GiB"
+}
