e2e32dbef73199b387fb7659f949f7af8cd9891e
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-05-09 15:13:09 -0500

    tofu fmt

--- infra/lxd-cluster/main.tf
+++ infra/lxd-cluster/main.tf
@@ -6,12 +6,12 @@ terraform {
 }
 
 module "control-plane" {
-  source            = "../modules/lxd-vms"
-  instance_count    = 1
-  name_prefix       = "control-plane"
-  cpu               = 4
-  memory            = "6GiB"
-  volume_size       = "80GiB"
+  source         = "../modules/lxd-vms"
+  instance_count = 1
+  name_prefix    = "control-plane"
+  cpu            = 4
+  memory         = "6GiB"
+  volume_size    = "80GiB"
 }
 
 module "worker" {
