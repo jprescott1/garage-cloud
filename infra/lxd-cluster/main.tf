ea6f66bb3bb7a733eb1892b92206c752f98a359c
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-05-09 11:00:10 -0500

    add worker nodes

--- infra/lxd-cluster/main.tf
+++ infra/lxd-cluster/main.tf
@@ -16,7 +16,7 @@ module "control-plane" {
 
 module "worker" {
   source         = "../modules/lxd-vms"
-  instance_count = 1
+  instance_count = 3
   name_prefix    = "worker"
   cpu            = 4
   memory         = "6GiB"
